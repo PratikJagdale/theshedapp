144864601acafed8ddeceb57cfea99fcb9a2b7ca
Pratik Jagdale <pratikjagdale111@gmail.com>
2020-11-25 21:26:10 +0530

    initial commit

---
+++ Project/theshed/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hi Bitches, let's do this without fighting!!!</h1>
+    </div>
+  );
+}
+
+export default App;
